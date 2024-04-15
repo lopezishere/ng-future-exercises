2a3503c4de4df64e1404f425a8422603e23139a7
lopezishere <jav.lopez.lopez@gmail.com>
2024-04-15 11:01:39 +0000

    Ejercicio Formularios

--- src/app/exercises/components/forms/forms.component.ts
+++ src/app/exercises/components/forms/forms.component.ts
@@ -1,6 +1,13 @@
+import { formatDate } from '@angular/common'
 import { Component } from '@angular/core'
-import { ReactiveFormsModule } from '@angular/forms'
+import { FormBuilder, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 
+interface CreateHabit {
+  nombre: string
+  fecha: Date
+}
+
+//type CreteHabitFormModel = FormModel<CreateHabit>
 @Component({
   selector: 'app-forms',
   standalone: true,
@@ -8,4 +15,43 @@ import { ReactiveFormsModule } from '@angular/forms'
   styleUrl: './forms.component.css',
   imports: [ReactiveFormsModule],
 })
-export class FormsComponent {}
+export class FormsComponent {
+  habitos: {
+    nombre: string
+    fecha: string
+  }[] = []
+
+  mensajeError: string = ''
+
+  /* API FormBuilder - Recomendada */
+  // Al poner nonNullable, todas las variables generadas no podrán ser nulas
+  fb = new FormBuilder().nonNullable
+  formulario = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(2)]],
+  })
+
+  /* API Form Groups  
+  formulario =  new FormGroup({    
+    nombre: new FormControl('', [Validators.required, Validators.min(2)])
+  })
+  */
+
+  crearHabito() {
+    console.log(this.formulario)
+    if (this.formulario.valid) {
+      this.mensajeError = ''
+      this.habitos.push({
+        nombre: this.formulario.value.nombre ?? '',
+        fecha: formatDate(Date.now(), 'MMM dd, YYYY', 'en-US'),
+      })
+      this.formulario.reset()
+    } else {
+      if (this.formulario.controls.nombre.errors?.['required']) {
+        this.mensajeError = 'El nombre del hábito es obligatorio'
+      }
+      if (this.formulario.controls.nombre.errors?.['minlength']) {
+        this.mensajeError = 'El nombre del hábito debe tener, al menos, 2 letras'
+      }
+    }
+  }
+}
